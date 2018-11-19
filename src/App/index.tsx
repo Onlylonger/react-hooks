9162bf5ae0490fffc17b685010b31af0edc02be8
王世隆 <547463736@qq.com>
2018-11-19 17:07:55 +0800

    chore: 初始化项目
    	webpack 构建
    		typesciprt 编译
    		cssnext 编译
    	prettier 和 stylelint,tslint 格式化配置

---
+++ src/App/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Style from './style.css'
+
+const App = () => {
+  return (
+    <div className={Style.app}>
+      nihao
+      <div className={Style.banner}>banner</div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+import App from './App/index'
+import './style/_reset.css'
+
+render(<App />, document.getElementById('app'))
